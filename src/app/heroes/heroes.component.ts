c802754d09829b7305ca3e6b4c8a36d004d344f8
Ken <kmartinez@sjcoe.net>
2023-04-25 11:53:29 -0700

    Built message component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../interfaces/hero';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
@@ -9,20 +10,25 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   // hero: Hero = {
   //   id: 1,
   //   name: 'Windstorm'
   // };
+ 
   selectedHero?: Hero;
 
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   // getHeroes(): void {
@@ -34,7 +40,5 @@ export class HeroesComponent {
         .subscribe(heroes => this.heroes = heroes);
   }
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+  
 }
